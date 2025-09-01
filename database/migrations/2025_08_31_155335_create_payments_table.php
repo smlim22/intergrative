62316439b028936860d05d52ec3e1da3312c1e5d
AdreanG <adreangoh@gmail.com>
2025-09-01 22:50:47 +0800

    Payment and reservation models

---
+++ database/migrations/2025_08_31_155335_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up()
+{
+    Schema::create('payments', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('reservation_id')->constrained()->onDelete('cascade');
+        $table->decimal('amount', 10, 2);
+        $table->string('currency')->default('MYR');
+        $table->string('payment_method')->default('paypal');
+        $table->string('payment_status')->default('pending');
+        $table->string('transaction_id')->nullable();
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
